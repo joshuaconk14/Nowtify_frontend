423ccd4dad3fab63555269cd9fa314b00f75ca92
Joshua Conklin <joshuaconk14>
2024-11-01 17:10:14 -0700

    app running

--- src/App.js
+++ src/App.js
@@ -2,31 +2,69 @@ import React, {useState, useEffect} from 'react'
 
 function App() {
 
-  const[data,setData] = useState([{}])
+//   const[data,setData] = useState([{}])
 
-  useEffect(() => {
-    fetch("/members").then(
-      res => res.json()
-    ).then(
-        data => {
-          setData(data)
-          console.log(data)
-      }
-    )
-  }, [])
+//   useEffect(() => {
+//     fetch("/members").then(
+//       res => res.json()
+//     ).then(
+//         data => {
+//           setData(data)
+//           console.log(data)
+//       }
+//     )
+//   }, [])
 
-  return (
-    <div>
+//   return (
+//     <div>
       
-        {(typeof data.members === 'undefined') ? (
-            <p>Loading...</p>
-        ) : (
-          data.members.map((member, i) => (
-              <p key={i} > {member} </p>
-          ))
-        )}
-    </div>
-  )
+//         {(typeof data.members === 'undefined') ? (
+//             <p>Loading...</p>
+//         ) : (
+//           data.members.map((member, i) => (
+//               <p key={i} > {member} </p>
+//           ))
+//         )}
+//     </div>
+//   )
+// }
+
+  // BACKEND API
+
+  // returning string in useState
+  const [message, setMessage] = useState("");
+
+useEffect(() => {
+  fetch("/home")
+    .then(res => res.json())
+    .then(message => {
+      setMessage(message);
+      // console.log(message);
+    });
+}, []);
+
+  // HTML API
+const [htmlContent, setHtmlContent] = useState("");
+
+useEffect(() => {
+  fetch(`${process.env.PUBLIC_URL}/home.html`)
+    .then(res => res.text())
+    .then((html) => {
+      setHtmlContent(html); // Store the HTML in state
+    })
+    .catch((error) => console.error("Error loading HTML:", error));
+}, []);
+
+return (
+  <div className="App">
+    <h1>Nowtify</h1>
+    <div dangerouslySetInnerHTML={{ __html: htmlContent }} />
+  </div>
+);
+
+
 }
+
+
 
 export default App
