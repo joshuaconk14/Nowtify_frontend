c567789c71ed7e9a09dc661ec6fb8d3f04fe89da
Joshua Conklin <joshuaconk14>
2024-11-02 19:45:41 -0700

    name change

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App2';
-import reportWebVitals from './reportWebVitals2';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
