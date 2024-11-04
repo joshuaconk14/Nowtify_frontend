66b26b9f977322e39ba2b3da6116d1043a2c536c
Joshua Conklin <joshuaconk14>
2024-11-04 01:11:38 -0800

    login more

--- src/pages/login.js
+++ src/pages/login.js
@@ -4,8 +4,10 @@ import './login.css'
 function Login() {
   return (
     <div>
+      <div class="big-rectangle">
       <h1 class="login-header"> Login</h1>
       <p class="welcome-back-text">Welcome back!</p>
+      </div>
     </div>
   );
 }
