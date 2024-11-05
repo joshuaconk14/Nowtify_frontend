4a0f707f53c15c83b4a0490b87a10df853a47701
Joshua Conklin <joshuaconk14>
2024-11-05 14:50:36 -0800

    spotify login button

--- src/pages/login.js
+++ src/pages/login.js
@@ -7,4 +7,22 @@ function Login() {
 
   // use window.location.href when linking to pages in diff venv
     // return null says to React don't run anything w < Navigate />
-      // window.location.hr
+      // window.location.href will handle redirection
+  if (goToUserAuth) {
+    window.location.href = "http://127.0.0.1:5003/spotify-login";
+  }
+
+  return (
+    <div>
+      <div class="big-rectangle">
+      <h1 class="login-header"> Login</h1>
+      <p class="welcome-back-text">Welcome back!</p>
+      <button class="username-box"> username</button>
+      <button class="password-box">password</button>
+      <button onClick = { () => setGoToUserAuth(true)} class = "spotify-login-button" >Login through Spotify</button>
+      </div>
+    </div>
+  );
+}
+
+export default Login
