12f25c26481516e099683f01d376da5a0d0e051a
Joshua Conklin <joshuaconk14>
2024-11-07 14:58:25 -0800

    login button

--- src/pages/login.js
+++ src/pages/login.js
@@ -3,7 +3,10 @@ import './login.css';
 
 function Login() {
 
+  // state variable for spotify login
   const [goToUserAuth, setGoToUserAuth] = React.useState(false)
+
+
 
   // use window.location.href when linking to pages in diff venv
     // return null says to React don't run anything w < Navigate />
@@ -13,16 +16,24 @@ function Login() {
   }
 
   return (
-    <div>
-      <div class="big-rectangle">
-      <h1 class="login-header"> Login</h1>
-      <p class="welcome-back-text">Welcome back!</p>
-      <button class="username-box"> username</button>
-      <button class="password-box">password</button>
-      <button onClick = { () => setGoToUserAuth(true)} class = "spotify-login-button" >Login through Spotify</button>
+    <div className="container">
+      <div className="big-rectangle">
+        <h1 className="login-header">Login</h1>
+        <p className="welcome-back-text">Welcome back!</p>
+        
+        <form>
+          <input type="text" name="username" placeholder="username" className="username-box" />
+          <input type="password" name="password" placeholder="password" className="password-box" />
+          <input type="submit" value="login" className="login-submit" />
+        </form>
+
+        <button onClick={() => setGoToUserAuth(true)} className="spotify-login-button">
+          Login through Spotify
+        </button>
       </div>
     </div>
   );
 }
+
 
 export default Login
